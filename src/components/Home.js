b86b1cb6b4524f878181ac5b04f044e9a3da355d
John <johnmulvey48@yahoo.com>
2022-03-22 10:35:50 -0400

    git@github.com:Jmulvez/react-hooks-jsx-lab.git

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
-function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+const divStyle = {
+  color: 'firebrick',
+};
+
+function ComponentWithTransition() {
+  return <h1 style={divStyle}></h1>;
 }
+
+function Home() {
+  return (
+    <div id="home">
+      <h1 style="firebrick">
+        {username} is a Web Developer from {city}
+      </h1>
+    </div>
+  )}
 
 export default Home;
